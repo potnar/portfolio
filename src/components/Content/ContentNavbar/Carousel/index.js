a5b3d4327d0ad6c12b4e460f90e1e9e250a27247
idoe <idoe@localhost.localdomain>
2020-03-23 21:34:00 +0100

    feature: carousel

--- src/components/Content/ContentNavbar/Carousel/index.js
+++ src/components/Content/ContentNavbar/Carousel/index.js
@@ -2,15 +2,33 @@ import React from "react";
 import "./Carousel.scss";
 
 const Carousel = props => {
+  if (!props.items) {
+    return null;
+  }
   return (
-    <div className="carousel" style={{ "--step": props.step }}>
-      <div className="carousel__item"></div>
-      <div className="carousel__item"></div>
-      <div className="carousel__item"></div>
-      <div className="carousel__item"></div>
-      <div className="carousel__item"></div>
+    <div className="carousel__wrapper">
+      <div className="carousel" style={{
+        "--step": props.step,
+        "--n": props.items.length
+      }}>
+        {props.items.map(item => (
+          <div className="carousel__item">
+           <div className="carousel__item__content">
+            {item.content || null}
+           </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
+};
+
+Carousel.defaultProps = {
+  items: [
+    { content: <div>lalalala</div> },
+    {},
+    {}
+  ]
 };
 
 export default Carousel;
