8e028fd362686e7f52d8ecd9e9badf55a942bd85
potnar <potnar6@users.noreply.github.com>
2020-08-10 18:29:56 +0200

    add build

---
+++ build/precache-manifest.8a463e630244505a04885cb4b453cdc8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6cd65a0119374ac6e9988ac8563663d",
+    "url": "/potnar/portfolio/index.html"
+  },
+  {
+    "revision": "3dbe26a52548c23671d2",
+    "url": "/potnar/portfolio/static/css/main.952354d0.chunk.css"
+  },
+  {
+    "revision": "7b9185fae3a353f584b8",
+    "url": "/potnar/portfolio/static/js/2.ff8b86ac.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/potnar/portfolio/static/js/2.ff8b86ac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3dbe26a52548c23671d2",
+    "url": "/potnar/portfolio/static/js/main.914e8fce.chunk.js"
+  },
+  {
+    "revision": "a3ce9ff9cb920818c4db",
+    "url": "/potnar/portfolio/static/js/runtime-main.29ebde1f.js"
+  },
+  {
+    "revision": "4838b535296a4fe73b0f33071e760718",
+    "url": "/potnar/portfolio/static/media/BebasNeueOTF.4838b535.otf"
+  },
+  {
+    "revision": "a8593658a077d59b311171d6a0d19271",
+    "url": "/potnar/portfolio/static/media/Logo2.a8593658.svg"
+  },
+  {
+    "revision": "c97a9fc29652bb4afcdac68020e5d0f7",
+    "url": "/potnar/portfolio/static/media/Merriweather-Regular.c97a9fc2.ttf"
+  },
+  {
+    "revision": "12c3f8b781efebeb80db747e92e27e0b",
+    "url": "/potnar/portfolio/static/media/code_logo.12c3f8b7.png"
+  },
+  {
+    "revision": "436193f8431ba7adc217950d25ab32d7",
+    "url": "/potnar/portfolio/static/media/css3_logo.436193f8.png"
+  },
+  {
+    "revision": "817a688380d07c891278eb1d4a978307",
+    "url": "/potnar/portfolio/static/media/html5_logo.817a6883.webp"
+  },
+  {
+    "revision": "dd0345a329b75a4f8706dc3284bb22e8",
+    "url": "/potnar/portfolio/static/media/komp.dd0345a3.svg"
+  },
+  {
+    "revision": "e1ffad45cbd33238d3d1512d3e902fa4",
+    "url": "/potnar/portfolio/static/media/php7_logo.e1ffad45.png"
+  }
+]);
