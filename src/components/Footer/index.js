7f853d78391dbe94960485c9dda5fa30dd77c269
potnar <potnar6@users.noreply.github.com>
2020-08-10 11:38:27 +0200

    add Footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,51 @@
 import React from "react";
 import "./Footer.scss";
+import {
+  AiOutlineFacebook,
+  AiOutlineLinkedin,
+  AiOutlineGithub,
+} from "react-icons/ai";
+import { IconContext } from "react-icons";
 
-const Header = () => {
+const Footer = () => {
   return (
-    <footer>
-      <div className="footer-content"></div>
+    <footer className="footer-content">
+      <div className="container">
+        <div className="col">
+          <div className="socials">
+            <IconContext.Provider value={{ className: "icons" }}>
+              <a href="https://www.linkedin.com/in/mikołaj-cholewa-4b909573/">
+                <AiOutlineLinkedin size="1.6rem" />
+              </a>
+              <a href="https://github.com/potnar">
+                <AiOutlineGithub size="1.6rem" />
+              </a>
+              <a href="https://pl-pl.facebook.com/people/Miko%C5%82aj-Cholewa/100000746907733/">
+                <AiOutlineFacebook size="1.6rem" />
+              </a>
+            </IconContext.Provider>
+          </div>
+        </div>
+        <div className="col">
+          <div className="links">
+            <h3>Portfolio:</h3>
+            <p>
+              <a href="https://github.com/potnar/portfolio">
+                github.com/potnar/portfolio
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className="col">
+          <div className="address">
+            <h3>Mikołaj Cholewa</h3>
+            <p>Opaczewska 36/11 </p>
+            <p>Warsaw</p>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
 
-export default Header;
+export default Footer;
