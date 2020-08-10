5b346d780347656044df5dcbc9a37200eead9a8d
potnar <potnar6@users.noreply.github.com>
2020-08-10 11:34:48 +0200

    create About component

---
+++ src/components/AboutMe/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./About.scss";
+
+const AboutMe = () => {
+  return (
+    <main className="about-section" id="about-section">
+      <nav>About</nav>
+      <div className="container">
+        <div className="col">
+          <div className="bg-text">ABOUT</div>
+          <article>
+            <p>
+              I'm Mikołaj (if you want to Nicolas ;) ), Frontend Developer from
+              a passion. I'm living in Warsaw near to Szczesliwicki Park.
+            </p>
+            <p>
+              I studied on Electronic and Telecomunication faculty of studies on
+              Warsaw University of Technology.
+            </p>
+            <p>
+              My hobbies are computer games, riding a bicycle, volleyball,
+              football(soccer) and cars.
+            </p>
+          </article>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default AboutMe;
